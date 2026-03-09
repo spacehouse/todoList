9982370edfe7b867f3c6bffba82c2ce53246f49f
xiaoyu <1099428989@qq.com>
2026-03-10 00:57:39 +0800

    chore: 更新构建脚本和gitignore配置

--- build-with-java17.bat
+++ build-with-java17.bat
@@ -1,5 +1,7 @@
 @echo off
-set JAVA_HOME=D:\JAVA\JDK\jdk-17.0.4
-set PATH=%JAVA_HOME%\bin;%PATH%
+if "%JAVA_HOME%"=="" (
+    set "JAVA_HOME=D:\JAVA\JDK\jdk-17.0.4"
+)
+set "PATH=%JAVA_HOME%\bin;%PATH%"
 cd /d "%~dp0"
 call gradlew.bat %*
